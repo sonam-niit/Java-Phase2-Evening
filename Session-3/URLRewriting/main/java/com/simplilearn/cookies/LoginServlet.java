ce7a839daed32b2188d56be9ae0ead6a00397ac0
unknown <sonam.gravity@gmail.com>
2022-06-15 21:19:51 +0530

    url rewriting

---
+++ Session-3/URLRewriting/main/java/com/simplilearn/cookies/LoginServlet.java
@@ -0,0 +1,38 @@
+package com.simplilearn.cookies;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname= request.getParameter("uname");
+		String pass= request.getParameter("pass");
+		PrintWriter out= response.getWriter();
+		if(pass.equals("admin123"))
+		{
+			out.print("Successfully Logged In ..!!!<br>");
+			//let's form URL rewriting to pass user details to next servlet
+			out.print("<a href='dashboard?user="+uname+"'>Click here to access Dashboard</a>");
+			
+		}
+		else
+		{
+			out.print("Wrong username or password");
+		}
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
