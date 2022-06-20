a8d822985e25c915628f5512d0c61531d79cccdb
unknown <sonam.gravity@gmail.com>
2022-06-20 23:02:31 +0530

    hibernate web app

---
+++ Session-6/HibernateWebApp/main/java/com/simplilearn/demo/Student.java
@@ -0,0 +1,58 @@
+package com.simplilearn.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Table(name = "studentdata")
+public class Student {
+
+	@Id //Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Auto generation
+	@Column(name = "sid") //if the column name is different then the field name
+	private int id;
+	@Column(name="sname")
+	private String name;
+	@ColumnDefault("NULL")
+	private String email;
+	@ColumnDefault("NULL")
+	private String password;
+	public Student() {}
+	public Student(String name, String email, String password) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ Session-6/HibernateWebApp/main/java/com/simplilearn/demo/TestServlet.java
@@ -0,0 +1,37 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		PrintWriter out= response.getWriter();
+		SessionFactory factory= HibernateUtil.getSessionFactory();
+		Session session= factory.openSession();
+		Transaction tx= session.beginTransaction();
+		
+		session.save(new Student("pooja","p@gmail.com","223462"));
+		tx.commit();
+		session.close();
+		out.print("Data Inserted");
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
